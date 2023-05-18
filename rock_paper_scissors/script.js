563362b16faade28207e4bacdf224a6a7c509bfb
abhishek <abiswah.2000@gmail.com>
2023-05-19 00:20:54 +0530

    First console-only version of the `rock_paper_scissors` game project.

--- rock_paper_scissors/script.js
+++ rock_paper_scissors/script.js
@@ -9,36 +9,12 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)]; // returns a random choice from the available choices
   }
 
-// console.log(getComputerChoice());
-
-function playRound(playerSelection, computerSelection) {
-    // your code here!
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-
-    if (playerSelection === computerSelection) {
-        return "It's a tie!";
-    }
-    else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You win! Rock beats scissors.";
-    }
-    else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You win! Paper beats rock.";
-    }
-    else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You win! Scissors beats paper.";
-    }
-    else {
-        return "You lose! " + computerSelection + " beats " + playerSelection;
-    }
-  }
-
 /**
  * Prompts the user for a choice of "rock", "paper", or "scissors" until a valid choice is made.
  * 
  * @returns {string} - A user-selected choice of "rock", "paper", or "scissors"
  */
-function promptUserChoice() {
+function getUserChoice() {
     const validChoices = {
         'r': 'rock',
         'p': 'paper',
@@ -54,19 +30,51 @@ function promptUserChoice() {
 
     if (!Object.keys(validChoices).includes(userChoice)) {
         alert(`Invalid choice. Please enter 'r' for rock, 'p' for paper, or 's' for scissors.`);
-        return promptUserChoice();
+        return getUserChoice();
     }
 
     return validChoices[userChoice];
 }
 
-let getPlayerSelection = promptUserChoice();
+function game() {
 
-if (getPlayerSelection === null) {
-    console.log("The user chose to exit the game.");
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        playerSelection = getUserChoice();
+        computerSelection = getComputerChoice();
+
+        if (playerSelection === null) {
+            console.log("The user chose to exit the game.");
+            break;
+        }
+
+        if (playerSelection === computerSelection) {
+            console.log("Tie!");
+        } else if (
+            (playerSelection === "rock" && computerSelection === "scissors") ||
+            (playerSelection === "paper" && computerSelection === "rock") ||
+            (playerSelection === "scissors" && computerSelection === "paper")
+        ) {
+            playerScore += 1;
+            console.log("You win this round!");
+        } else {
+            computerScore += 1;
+            console.log("Computer wins this round!");
+        }
+    }
+    console.log(`Player score: ${playerScore} Computer score: ${computerScore}`);
+    
+    if (playerScore > computerScore) {
+        console.log("You win!");
+    }
+    else if (playerScore < computerScore) {
+        console.log("Computer wins!");
+    }
+    else {
+        console.log("It's a draw!");
+    }
 }
 
-console.log(getPlayerSelection);
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
+game();
